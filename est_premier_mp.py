3164988f9ac3577471626cefa072f1d582ad5834
mugiebros <72838745+mugiebros@users.noreply.github.com>
2020-12-16 23:44:11 -0500

    fin multi

--- est_premier_mp.py
+++ est_premier_mp.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import est_premier_seq
 import sys
+import multiprocessing as mp
 from multiprocessing import Process, Value
 from typing import NoReturn, List
 import math
@@ -23,9 +24,9 @@ def parse_arg() -> argparse.Namespace:
     """
     parser = argparse.ArgumentParser(description="Commande de nombres premiers -- @2020, Par Austin Brodeur")
     parser.add_argument('nombre', help='nombre à traiter pour connaitre sa primalité', nargs='?',type=int)
-    parser.add_argument('-d', '--délai',metavar='DÉLAI', help='delai du calcul (Défaut 10 sec)')
+    parser.add_argument('-d', '--délai',metavar='DÉLAI', help='delai du calcul (Défaut 10 sec)',default=10)
     parser.add_argument('-e', '--explication',metavar='', help="Expliquer pourquoi non premier")
-    parser.add_argument('-p', '--processus', type=int, metavar='PROCESSUS',default=4,
+    parser.add_argument('-p', '--processus', type=int, metavar='PROCESSUS',default=mp.cpu_count(),
                         help='nombres de processus à utiliser')
     parser.add_argument('-t','--tarce',action='store_true',help="Activer la trace d'éxecution")
     parser.add_argument('-q','--quiet',action='store_true',help="Ne pas afficher les processus")
@@ -66,39 +67,42 @@ def est_premier_mp(nombre: int,rangnumb: int) -> None:
 def main(argv: List[str]) -> None:
     début = timer()
     parse_arg()
+    processus = []
     if parse_arg().nombre <= 100 or parse_arg().nombre % 2 == 0:
         est_premier_seq.main(argv)
     else:
         try:
-            ps1 = Process(target=est_premier_mp,args=(parse_arg().nombre,3))
-            ps2 = Process(target=est_premier_mp, args=(parse_arg().nombre, 5))
-            ps3 = Process(target=est_premier_mp, args=(parse_arg().nombre, 7))
-            ps4 = Process(target=est_premier_mp, args=(parse_arg().nombre, 9))
-            ps1.start()
-            if not parse_arg().quiet:
-                print(" * pid ", ps1.pid, " -- range(", 3, ",", math.isqrt(parse_arg().nombre) + 1, ",", 8, ")")
-            ps2.start()
-            if not parse_arg().quiet:
-                print(" * pid ", ps2.pid, " -- range(", 5, ",", math.isqrt(parse_arg().nombre) + 1, ",", 8, ")")
-            ps3.start()
-            if not parse_arg().quiet:
-                print(" * pid ", ps3.pid, " -- range(", 7, ",", math.isqrt(parse_arg().nombre) + 1, ",", 8, ")")
-            ps4.start()
-            if not parse_arg().quiet:
-                print(" * pid ", ps4.pid, " -- range(", 8, ",", math.isqrt(parse_arg().nombre) + 1, ",", 8, ")")
-            ps1.join(5)
-            ps2.join(5)
-            ps3.join(5)
-            ps4.join(5)
-            if ps1.is_alive() or ps1.is_alive() or ps1.is_alive() or ps1.is_alive():
-                raise TimeoutError(f"Le delai de {DELAI_SEC} secondes est écoulé")
+            process = parse_arg().processus
+            rendu = 3
+            ouiounon = True
+            if parse_arg().processus == 0:
+                process = mp.cpu_count()
+            if parse_arg().processus < 0:
+                process = mp.cpu_count() + parse_arg().processus
+            for i in range(0,process):
+                pro = Process(target=est_premier_mp, args=(parse_arg().nombre, rendu))
+                pro.start()
+                processus.append(pro)
+                rendu+=2
+            rendu = 3
+            for i in processus:
+                i.join(parse_arg().délai)
+                if not parse_arg().quiet:
+                    print(" * pid ", i.pid, " -- range(", rendu, ",", math.isqrt(parse_arg().nombre) + 1, ",", 8,
+                            ")")
+                if i.is_alive():
+                    raise TimeoutError(f"Le delai de {parse_arg().délai} secondes est écoulé")
+                if i.exitcode == 1:
+                    ouiounon = False
+                rendu+=3
+
+            if ouiounon:
+                print(Fore.CYAN + "Selon Austin Brodeur: " + Fore.RESET + "Oui")
+                print(Fore.YELLOW + "Durée:", timer() - début, "sec")
             else:
-                if ps1.exitcode == 0 and ps2.exitcode == 0 and ps3.exitcode == 0 and ps4.exitcode == 0:
-                    print(Fore.CYAN + "Selon Austin Brodeur: " + Fore.RESET + "Oui")
-                    print(Fore.YELLOW + "Durée:", timer() - début, "sec")
-                else:
-                    print(Fore.CYAN + "Selon Austin Brodeur: " + Fore.RESET + "Non")
-                    print(Fore.YELLOW + "Durée:", timer() - début, "sec")
+                print(Fore.CYAN + "Selon Austin Brodeur: " + Fore.RESET + "Non")
+                print(Fore.YELLOW + "Durée:", timer() - début, "sec")
+
         except KeyboardInterrupt:
             exexit(KeyboardInterrupt("Interruption Clavier"))
             print(Fore.MAGENTA + "Durée:", timer() - début, "sec")
